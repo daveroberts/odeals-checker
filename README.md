31b2b99fb53c1ac285cded9f7f98e8a22bd22643
Dave Roberts <dave.a.roberts@gmail.com>
2024-03-24 10:16:21 -0400

    WIP

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
-# Check-And-Notify
-
-Will run a script, check something, then send out an email, automatically with github actions
+# Odeals-Checker
 
 ## Setup
+
+Must create a `.env` and watchlist.csv file. There is a sample of each you can use
 
 The following configuration variables are used:
 
     SMTP_HOSTNAME
+    SMTP_PORT
     SMTP_USERNAME
     SMTP_PASSWORD
 
-For gmail, the SMTP hostname is "smtp.gmail.com".  The username is your full email address including the @gmail.com.  If your Google account requires two factor authentication, your Google password won't work.  Instead, you'll need to use an application specific password.  Google appears to have removed the link to application specific passwords from the account security page, but as of March 2023, you can still create application specific passwords by this direct link to the [application specific password page](https://myaccount.google.com/apppasswords)
+For gmail, the SMTP hostname is "smtp.gmail.com". The username is your full email address including the @gmail.com. If your Google account requires two factor authentication, your Google password won't work. Instead, you'll need to use an application specific password. Google appears to have removed the link to application specific passwords from the account security page, but as of March 2023, you can still create application specific passwords by this direct link to the [application specific password page](https://myaccount.google.com/apppasswords)
 
 Add these variables to two places: a .env file locally in this repo and in github repo secrets
 
-The .env file should contain one line per configuration variable.  For example:
+The .env file should contain one line per configuration variable. For example:
 
     SMTP_HOSTNAME="smtp.gmail.com"
 
 ## Running locally
 
-    node start
+    npm start
 
 ## Setup Github actions
 
+Remove watchlist.csv from .gitignore
 In your github repo, click settings > Secrets and Variables > Actions
-Click "New Repository Secret".  "SMTP_HOSTNAME" for the name, and "smtp.gmail.com" for the secret.  Don't add quotes or newlines.  Repeat for all other secrets
+Click "New Repository Secret". "SMTP_HOSTNAME" for the name, and "smtp.gmail.com" for the secret. Don't add quotes or newlines. Repeat for all other secrets
 
 ## Run Github actions
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
-require('dotenv').config()
-const nodemailer = require('nodemailer')
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const { mkdirp } = require("mkdirp");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOSTNAME,
   port: process.env.SMTP_PORT || 465,
   secure: true,
   auth: {
     user: process.env.SMTP_USERNAME,
-    pass: process.env.SMTP_PASSWORD
-  }
+    pass: process.env.SMTP_PASSWORD,
+  },
 });
 
+const WAIT_SECONDS = 10;
 const DAY_SUNDAY = 0;
 const DAY_WEDNESDAY = 3;
 const DAY_THURSDAY = 4;
@@ -18,24 +23,95 @@ const HOUR_6AM = 10;
 const HOUR_12PM = 16;
 const HOUR_6PM = 22;
 
+let watchlist_txt = fs.readFileSync("watchlist.csv").toString();
+let watchlist_rows = watchlist_txt.split("\n").map((line) => line.split(","));
+watchlist = watchlist_rows.map((row) => {
+  return {
+    title: row[0],
+    url: row[1],
+    email_when_below: row[2],
+  };
+});
+
 (async function run() {
-  console.log('Running report...');
-  let now = new Date()
-  let day_of_month = now.getDate()
-  let day_of_week = now.getDay()
-  let hour = now.getHours()
-  if (day_of_week == DAY_SUNDAY && hour == HOUR_6PM){ await send_email("Task: Take out trash") }
-  if (day_of_week == DAY_WEDNESDAY && hour == HOUR_6PM){ await send_email("Task: Take out trash") }
-  if (day_of_month == 28 && hour == HOUR_6AM){ await send_email("Task: Pay Citi CC Bill") }
-  if (day_of_week == DAY_THURSDAY && hour == HOUR_12PM){ await send_email("Task: Epic Games free game of the week") }
-  if (day_of_week == DAY_SATURDAY && hour == HOUR_12PM){ await send_email("Task: Download Kindercare Pictures") }
+  console.log("Running report...");
+  let now = new Date();
+  let day_of_month = now.getDate();
+  let day_of_week = now.getDay();
+  let hour = now.getHours();
+  //await send_email("This is a test email");
+  await gather_prices();
 })();
 
-async function send_email(subject){
+async function gather_prices() {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  let prices = {};
+  for (let item of watchlist) {
+    let price = await gather_price(page, item);
+    prices[item.title] = price;
+    //await new Promise((r) => setTimeout(r, 10 * 1000));
+  }
+  await record_prices(prices);
+  await send_discount_email(prices);
+  await browser.close();
+}
+
+async function record_prices(prices) {
+  let keys = Object.keys(prices);
+  for (let title of Object.keys(prices)) {
+    let price = prices[title];
+    await mkdirp("history");
+    let filepath = path.join("history", `${title}.csv`);
+    fs.appendFileSync(filepath, `${new Date().toISOString()},${price}\n`);
+  }
+}
+
+async function send_discount_email(prices) {
+  let items_to_alert = [];
+  for (let title of Object.keys(prices)) {
+    let price = prices[title];
+    let item = watchlist.find((item) => item.title == title);
+    if (parseFloat(price) < parseFloat(item.email_when_below)) {
+      items_to_alert.push({ item, price });
+    }
+  }
+  if (items_to_alert.length > 0) {
+    await send_email(
+      "Meta Quest deals found",
+      items_to_alert
+        .map((struct) => `Title: ${struct.item.title}, Price: ${struct.price}`)
+        .join("\n")
+    );
+  }
+}
+
+async function gather_price(page, item) {
+  console.log(`Trying to gather price for ${item.title}`);
+  await page.goto(item.url, { waitUntil: "domcontentloaded" });
+  await new Promise((r) => setTimeout(r, WAIT_SECONDS * 1000));
+  let price = await page.evaluate(() => {
+    let price_boxes = document.getElementsByClassName("app-purchase-price");
+    let prices = Array.from(price_boxes).map((pb) => pb.innerText);
+    prices = prices.filter((p) => p.startsWith("$"));
+    prices = prices.map((p) => p.substring(1));
+    prices = [...new Set(prices)];
+    if (prices.length == 0) {
+      throw new Error(`No price found`);
+    }
+    return prices[0];
+  });
+  console.log(`${item.title} is $${price}`);
+  return price;
+}
+
+async function send_email(subject, text = "") {
   await transporter.sendMail({
     from: process.env.SMTP_USERNAME,
     to: process.env.SMTP_USERNAME,
     subject: subject,
-    text: ''
+    text: text,
   });
 }
